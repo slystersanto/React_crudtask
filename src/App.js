c730c68676965362eb815f09f400531bc9868327
slystersanto <santhosh.mech.19@gmail.com>
2023-02-23 10:04:50 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+ import './App.css';
+ import "./components/StudentProfile.css"
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router ,Switch ,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Reports from './Pages/Reports';
+import  StudentDetails  from './components/Student';
+import { data } from './Data/data';
+import StudentProfile from './components/StudentProfile';
+import AddStudents from './components/AddStudents';
+import EditStudents from './components/EditStudents';
+
 
 function App() {
+  const [studentsData, setStudents] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+        <Route path='/' exact component={Home} />
+
+          <Route path='/batchdetails'>
+            <StudentDetails studentsData={studentsData} setStudents={setStudents}  />
+            </Route>
+             
+          <Route path='/student/:id'>
+            <StudentProfile studentsData={studentsData}/>
+              </Route>
+
+              <Route path='/add-student'>
+            <AddStudents studentsData={studentsData} setStudents={setStudents} />
+              </Route>
+
+              
+              <Route path='/edit/:id'>
+            <EditStudents studentsData={studentsData} setStudents={setStudents} />
+              </Route>
+
+
+        </Switch>
+      </Router>
+    </>
   );
 }
 
